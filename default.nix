74a5a74c65e6a3857229d2720aab865df93f2844
Eric Bailey <eric@ericb.me>
2018-08-18 12:23:10 -0500

    Add more packages

--- default.nix
+++ default.nix
@@ -1,14 +1,16 @@
 { pkgs ? import <nixpkgs> {} }:
 
-{
+rec {
 
-  browserpass = pkgs.callPackage ../pkgs/tools/security/browserpass {};
+  browserpass = pkgs.callPackage ./pkgs/tools/security/browserpass {};
 
   erlang = pkgs.beam.interpreters.erlangR20.override {
     enableDebugInfo = true;
     installTargets = "install";
     wxSupport = false;
   };
+
+  git-crypt = pkgs.callPackage ./pkgs/applications/version-management/git-and-tools/git-crypt {};
 
   lab = pkgs.callPackage ./pkgs/applications/version-management/git-and-tools/lab {};
 
@@ -22,5 +24,15 @@
       sha256 = "0lql4q946gna2pl1g409mmmsvn2qvnq2z5cihrkfhk7plcqdny9n";
     };
   });
+
+  nodejs = pkgs.nodejs-8_x;
+
+  nodePackages = pkgs.nodePackages //
+    pkgs.callPackage ./pkgs/development/node-packages {
+      inherit pkgs;
+      inherit nodejs;
+    };
+
+  sourcetree = pkgs.callPackage ./pkgs/os-specific/darwin/sourcetree {};
 
 }
