1f9ac591a50499bd02f51293128f81579c41ab15
Eric Bailey <e.bailey@sportradar.com>
2018-08-20 12:46:11 -0500

    Move top-level packages to pkgs

--- default.nix
+++ default.nix
@@ -1,21 +1,25 @@
 { pkgs ? import <nixpkgs> {} }:
 
-rec {
+{
 
   overlays = import ./overlays;
 
-  browserpass = pkgs.callPackage ./pkgs/tools/security/browserpass {};
+  pkgs = {
 
-  erlang = pkgs.beam.interpreters.erlangR20.override {
-    enableDebugInfo = true;
-    installTargets = "install";
-    wxSupport = false;
+    browserpass = pkgs.callPackage ./pkgs/tools/security/browserpass {};
+
+    erlang = pkgs.beam.interpreters.erlangR20.override {
+      enableDebugInfo = true;
+      installTargets = "install";
+      wxSupport = false;
+    };
+
+    git-crypt = pkgs.callPackage ./pkgs/applications/version-management/git-and-tools/git-crypt {};
+
+    lab = pkgs.callPackage ./pkgs/applications/version-management/git-and-tools/lab {};
+
+    sourcetree = pkgs.callPackage ./pkgs/os-specific/darwin/sourcetree {};
+
   };
-
-  git-crypt = pkgs.callPackage ./pkgs/applications/version-management/git-and-tools/git-crypt {};
-
-  lab = pkgs.callPackage ./pkgs/applications/version-management/git-and-tools/lab {};
-
-  sourcetree = pkgs.callPackage ./pkgs/os-specific/darwin/sourcetree {};
 
 }
