4e5d6e7116064532bbb06226d87b5a57394b80a9
Eric Bailey <eric@ericb.me>
2019-01-17 17:01:15 -0600

    Upgrade k8s tools
    Mark Helm 2.12.2 TODO for now.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,7 +11,7 @@ in
 
 rec {
 
-  inherit (_nixpkgs) autojump kops kubernetes-helm kubetail;
+  inherit (_nixpkgs) autojump kubetail;
   inherit (_nixpkgs.gitAndTools) git-crypt;
 
   erlang = pkgs.beam.interpreters.erlangR20.override {
@@ -28,9 +28,61 @@ rec {
     withGraphics = false;
   };
 
-  kubectx = pkgs.callPackage ./applications/networking/cluster/kubectx {};
+  kops = _nixpkgs.kops.overrideAttrs(oldAttrs: rec {
+    name = "kops-${version}";
+    version = "1.11.0";
+    src = _nixpkgs.fetchFromGitHub {
+      owner = "kubernetes";
+      repo = "kops";
+      rev = version;
+      sha256 = "1z67jl66g79q6v5kjy9qxx2xp656ybv5hrc10h3wmzy0b0n30s4n";
+    };
+  });
 
-  kubernetes = pkgs.callPackage ./applications/networking/cluster/kubernetes {};
+  kubectl = (kubernetes.override { components = [ "cmd/kubectl" ]; }).overrideAttrs(oldAttrs: rec {
+    pname = "kubectl";
+    name = "${pname}-${oldAttrs.version}";
+  });
+
+  kubectx = (_nixpkgs.kubectx.override {
+    inherit kubectl;
+  }).overrideAttrs(oldAttrs: rec {
+    version = "0.6.2";
+    src = _nixpkgs.fetchFromGitHub {
+      owner = "ahmetb";
+      repo = oldAttrs.pname;
+      rev = version;
+      sha256 = "0kmzj8nmjzjfl5jgdnlizn3wmgp980xs6m9pvpplafjshx9k159c";
+    };
+  });
+
+  kubernetes = _nixpkgs.kubernetes.overrideAttrs(old: rec {
+    pname = "kubernetes";
+    name = "${pname}-${version}";
+    version = "1.11.6";
+    src = _nixpkgs.fetchFromGitHub {
+      owner = "kubernetes";
+      repo = "kubernetes";
+      rev = "v${version}";
+      sha256 = "0p4kh056m84gyh05zia38aa4fqqad78ark2cycbi3nb60jj1nl9a";
+    };
+  });
+
+  inherit (_nixpkgs) kubernetes-helm;
+
+  # TODO:
+  # kubernetes-helm = _nixpkgs.kubernetes-helm.overrideAttrs(oldAttrs: rec {
+  #   pname = "helm";
+  #   name = "${pname}-${version}";
+  #   version = "2.12.2";
+  #   goDeps = ./applications/networking/cluster/helm/deps.nix;
+  #   src = _nixpkgs.fetchFromGitHub {
+  #     owner = pname;
+  #     repo = pname;
+  #     rev = "v${version}";
+  #     sha256 = "0a8pajj9p080f4fbylf2jixkp8xmidx2vg7k02f0prrw6q26g6gf";
+  #   };
+  # });
 
   lab = pkgs.callPackage ./applications/version-management/git-and-tools/lab {};
 
