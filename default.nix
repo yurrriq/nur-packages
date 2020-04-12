30c6210a12fafd5d80852c8df4b3eae9daf2a997
Eric Bailey <eric@ericb.me>
2020-04-12 03:25:04 -0500

    Add kitty home-manager module

--- default.nix
+++ default.nix
@@ -1,6 +1,13 @@
 { pkgs ? import <nixpkgs> { } }:
 
+rec
 {
+  home-manager = rec {
+    modules = pkgs.lib.attrValues rawModules;
+    rawModules = import ./home-manager/modules;
+  };
+}
+// {
   lib = import ./lib;
   modules = import ./modules;
   overlays = import ./overlays;
