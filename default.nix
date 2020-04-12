831e68ea4ed47c4d5f4e744839ee8983bf5053ef
Eric Bailey <eric@ericb.me>
2020-04-12 02:10:04 -0500

    Remove unused lib functions

--- default.nix
+++ default.nix
@@ -1,15 +1,8 @@
 { pkgs ? import <nixpkgs> { } }:
-let
-  lib = import ./lib;
-in
+
 {
-
-  inherit lib;
-
+  lib = import ./lib;
   modules = import ./modules;
-
   overlays = import ./overlays;
-
-  pkgs = import ./pkgs { inherit lib pkgs; };
-
+  pkgs = import ./pkgs { inherit pkgs; };
 }
